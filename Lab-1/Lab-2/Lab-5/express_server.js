ce0b684821e04a312f8037ed1af1c88af599a3dd
Dharamdas yadav <gyandasyadav0536@gmail.com>
2026-09-08 21:26:05 +0530

    first

---
+++ Lab-1/Lab-2/Lab-5/express_server.js
@@ -0,0 +1,8 @@
+import express from "express";
+import dotenv from "dotenv";
+impot cars from "cars";
+const app = express()0;
+const userData = {
+{
+    id: 101
+}
---
+++ Lab-3/tempCodeRunnerFile.js
@@ -0,0 +1,59 @@
+import http from 'http'
+import { json } from 'stream/consumers';
+
+const userdata =[ {
+    name: 'Dharamdas yadav',
+    age: 20
+}];
+
+const server = http.createServer((req, res) => {
+    const url = req.url;
+    const method = req.method;
+    if (url === '/' && method === 'GET') {
+        res.statusCode = 200;
+        res.end('Welcome to the Home Page');
+
+    }
+    else if (url === '/sys' && method === 'GET') {
+        res.statusCode = 201;
+        res.end('Welcome to the System Page');
+    }
+    else if (url === '/data' && method === 'GET') {
+        res.statusCode = 200;
+        res.end(JSON.stringify(userdata));
+    }
+    else if(url === '/create' && method === 'POST'){
+        const body= " ";
+        res.on('data',(Chunk)=>{
+            body += Chunk;
+        });
+        res.on("end",()=> {
+            const newdata = json.parse(body);
+        
+            const newUserData={
+                name: newdata.name,
+                age: newdata.age
+            };
+            userdata.push(newUserData);
+            res.end('data update succesfull')
+        });
+    }
+    else if(url.startsWith('/users/')&& method === 'GET'){
+        const index = parseInt(url.split('/')[2]);
+        if(index >= 0 && index < userdata.length){
+            res.end(JSON.stringify(userdata[index]));
+        }
+        else {
+            res.statusCode = 404;
+            res.end('user not found');
+        }
+    }
+    else {
+            res.statusCode = 404;
+            res.end('Page not found');
+        }
+});
+
+server.listen(3000, () => {
+    console.log('Server running on http://localhost:3000');
+});
